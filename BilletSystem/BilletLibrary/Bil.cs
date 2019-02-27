f8f78643db3d6030d4b242a826b400138d4ef103
Emil Hammer <mr_h4mm3r@live.dk>
2019-02-27 10:35:50 +0100

    Added BilletLibrary

---
+++ BilletSystem/BilletLibrary/Bil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+            public string Nummerplade;
+            public DateTime dato;
+            public decimal Price = 240;
+            public string Køretøj = "Bil";
+    }
+}
