23bbb9cfb04972dc9b0ac7c912a8c87691460e2d
Emil Hammer <mr_h4mm3r@live.dk>
2019-02-27 10:37:37 +0100

    Added Library

---
+++ BilletSystem/UnitTestBilletSystem/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+
+namespace UnitTestBilletSystem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBilPrice()
+        {
+
+            
+        }
+    }
+}
